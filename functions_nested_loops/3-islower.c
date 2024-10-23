121f79a90f12c9daced14537fea63af6f83d6db6
Maryam <meryemshekiliyeva@gmail.com>
2024-10-23 10:55:07 +0400

    m

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -7,6 +7,12 @@
  */
 int _islower(int c)
 {
+/**
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
