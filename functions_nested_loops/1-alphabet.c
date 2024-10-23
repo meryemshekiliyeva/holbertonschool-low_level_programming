12ee1d48e8d7aadefc64492190cb789bb06b502b
Maryam <meryemshekiliyeva@gmail.com>
2024-10-23 10:29:07 +0400

    m

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,20 +1,15 @@
-/*
- * File: 1-alphabet.c
- */
-
 #include "main.h"
 
 /**
  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
  */
-
 void print_alphabet(void)
 {
-	char letter;
+    char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+    for (letter = 'a'; letter <= 'z'; letter++)
+        _putchar(letter);
 
-	_putchar('\n');
-        return (0);
+    _putchar('\n');
 }
+
