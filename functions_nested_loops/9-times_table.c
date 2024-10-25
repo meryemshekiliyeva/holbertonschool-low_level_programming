b58851111b481fccb764ea1e65bacd976148e4d7
Maryam <meryemshekiliyeva@gmail.com>
2024-10-25 10:54:59 +0400

    m

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- *times_table - Write a function that prints the 9 times table, starting with 0.
+ *times_table - Write a function that prints the 9 times table,
+ *              starting with 0.
  */
 void times_table(void)
 {
