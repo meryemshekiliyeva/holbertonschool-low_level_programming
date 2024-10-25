4a741e135c4c46d198f3a517995f1482874f5d09
Maryam <meryemshekiliyeva@gmail.com>
2024-10-25 10:59:23 +0400

    m

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add - Write a function that adds two integers
+ * and returns the result.
+ */
+int add(int a, int b)
+{
+	_putchar(a + b);
+	return (0);
+}
+
