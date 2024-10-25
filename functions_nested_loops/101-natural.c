f81bd47b678ce3f901550ca06e755db38dde3b49
Maryam <meryemshekiliyeva@gmail.com>
2024-10-25 11:35:44 +0400

    m

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -8,10 +8,9 @@
  */
 int main(void)
 {
-	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0; count < 50; count++)
+	for (int i = 0 : i < 50; i++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
@@ -19,7 +18,7 @@ int main(void)
 		fib1 = fib2;
 		fib2 = sum;
 
-		if (count == 49)
+		if (i == 49)
 			printf("\n");
 		else
 			printf(", ");
