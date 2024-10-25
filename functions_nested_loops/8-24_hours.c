f7612b1f07fc656043cb762e93ffc5d44c0e1f56
Maryam <meryemshekiliyeva@gmail.com>
2024-10-25 10:49:42 +0400

    m

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,11 +1,7 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
  * jack_bauer - Prints every minute of the day of
  *              Jack Bauer, starting from 00:00 to 23:59.
- * Return: Always 0.
  */
 void jack_bauer(void)
 {
