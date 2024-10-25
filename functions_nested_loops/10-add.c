f5499ad155f83060c463eba7e25f6466c8a73d2a
Maryam <meryemshekiliyeva@gmail.com>
2024-10-25 11:03:29 +0400

    m

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  * add - Adds two integers.
- * @num1: The first integer to be added.
- * @num2: The second integer to be added.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
  *
  * Return: The result of the addition.
  */
