1f5e0c625fbf0cd5ea7f1dd4e501c5521bdbbe6e
Maryam <meryemshekiliyeva@gmail.com>
2024-10-27 17:12:53 +0000

    m

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-    int sum = 0;
+	int i, result;
 
-    for (int i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-    }
-
-    printf("%d\n", sum);
-    return 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			result +=  i;
+		}
+	}
+	printf("result are:%d\n", result);
+	return (0);
 }
