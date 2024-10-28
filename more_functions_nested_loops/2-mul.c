0100df8f360cde6ce830e2a4f3aa3e0d5c765804
Maryam <meryemshekiliyeva@gmail.com>
2024-10-28 11:31:51 +0400

    m

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * @a - integer
- * @b - integer
- * mul - Write a function that multiplies two integers.
- * Return: Always 0.
+ *  mul - a function that multiplies two integers
+ * @a: first input
+ * @b: second input
+ * Return: multiplication result of the 2 inputs
  */
 int mul(int a, int b)
 {
