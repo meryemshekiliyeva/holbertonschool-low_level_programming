1edcc6cfcaf116aa47b8dca75de9136dd19a7f40
Maryam <meryemshekiliyeva@gmail.com>
2024-10-28 11:58:45 +0400

    m

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * print_most_numbers - Write a function that prints the numbers, from 0 to 9,
  *followed by a new line.
@@ -7,11 +6,11 @@
  */
 void print_most_numbers(void)
 {
-        int i;
-
-        for (i = 0; i <= 9; i++)
+	int i;
+	for (i = 0; i <= 9; i++){
 		if(i != 2 && i != 4){
-                _putchar(i + '0');
+		_putchar(i + '0');
+		}
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
