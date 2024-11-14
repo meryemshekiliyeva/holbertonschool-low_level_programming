6d8e2337d2bdd03b30fef0419b4c000e5f9407d1
Maryam <meryemshekiliyeva@gmail.com>
2024-11-14 13:19:03 +0400

    m

--- recursion/main.h
+++ recursion/main.h
@@ -12,5 +12,9 @@ int _sqrt(int n, int i);
 int wildcmp(char *s1, char *s2);
 int is_prime_number(int n);
 int is_palindrome(char *s);
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_match(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
 
 #endif
